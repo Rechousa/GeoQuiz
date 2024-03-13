2bd13e54bd0890550f92c5a46586ba4b8db56759
Pedro Martins <pedro.mb.martins@ctw.bmwgroup.com>
2024-03-13 00:44:17 +0000

    Change Toast by Snackbar

--- app/src/main/java/com/rechousa/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/rechousa/android/geoquiz/MainActivity.kt
@@ -3,8 +3,8 @@ package com.rechousa.android.geoquiz
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -18,11 +18,11 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
 
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+            Snackbar.make(view, R.string.correct_toast, Snackbar.LENGTH_SHORT).show()
         }
 
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            Snackbar.make(view, R.string.incorrect_toast, Snackbar.LENGTH_SHORT).show()
         }
     }
 }
